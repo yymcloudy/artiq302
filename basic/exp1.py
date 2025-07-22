c9081e2d283a182bf3b6870e74077c2c9974526c
yymcloudy <yangyuanming2025@gmail.com>
2025-07-23 07:33:59 +0800

    add trapenvscan demo

--- basic/exp1.py
+++ basic/exp1.py
@@ -6,7 +6,28 @@ import numpy as np
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from trapenv import TrapEnv
+from trapenv import TrapEnv, TrapEnvScan
+from ndscan.experiment import *
+
+class Example0(TrapEnvScan):
+    """example0_inherit_from_TrapEnvScan"""
+    
+    def build_fragment(self):
+        TrapEnvScan.build_fragment(self)
+        print("Example0 Build Done")
+    
+    @kernel
+    def prepare(self):
+        TrapEnvScan.prepare(self)
+        print("Example0 Prepare Done")
+    
+    @kernel
+    def run_once(self):
+        TrapEnvScan.run_once(self)
+        print("Example0 Run_Once Done")
+
+# Create scan experiment
+Example0Exp = make_fragment_scan_exp(Example0)
 
 class Example1(TrapEnv):
     """example1_inherit_from_TrapEnv"""
